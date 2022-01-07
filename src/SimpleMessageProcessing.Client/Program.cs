712c38cfba4e41dd9bc4c7ecccac0f4675c21282
Lim Xiang Rui <xiangrui.lim@appfuxion.com>
2022-01-07 10:08:02 +0800

    Fix bug for client.

--- src/SimpleMessageProcessing.Client/Program.cs
+++ src/SimpleMessageProcessing.Client/Program.cs
@@ -13,12 +13,6 @@ var formatA = @"{
     ""sendTime"": ""2021-08-23 09:00""
 }";
 var formatB = @"{
-    ""from"": ""6588882222"",
-    ""to"": ""6511111111"",
-    ""type"": ""text"",
-    ""text"": ""Hello"",
-    ""sendTime"": ""2021-08-23 09:00""
-}{
     ""message"": {
         ""type"": ""text"",
         ""id"": ""asdf-qwer-zxcv-aaaa"",
@@ -48,12 +42,12 @@ var response = await httpClient.PostAsync(apiUrl, content);
 response.EnsureSuccessStatusCode();
 
 //formatB
-content = new StringContent(formatA, Encoding.UTF8, "application/json");
+content = new StringContent(formatB, Encoding.UTF8, "application/json");
 response = await httpClient.PostAsync(apiUrl, content);
 response.EnsureSuccessStatusCode();
 
 //formatC
-content = new StringContent(formatA, Encoding.UTF8, "application/json");
+content = new StringContent(formatC, Encoding.UTF8, "application/json");
 response = await httpClient.PostAsync(apiUrl, content);
 response.EnsureSuccessStatusCode();
 
